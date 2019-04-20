75cd8fa9c5c0cd8660776e7dbf74a822a3841285
bradennicholson <braden@bradencnicholson.com>
2019-04-19 22:08:34 -0700

    Check if username exists with api/user/exists/:

--- api/user/exists.js
+++ api/user/exists.js
@@ -9,16 +9,28 @@ var url = env.mongourl;
 // Handle all requests with inputs
 router.get('/:request', function(req, res) {
   // Converting the request to JSON
-  //var jsonRequest = JSON.parse(req.params.request);
   MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("wikit");
-  dbo.collection("user").findOne({}, function(err, result) {
     if (err) throw err;
-    res.json({ result: result });
-    db.close();
+    var dbo = db.db("wikit");
+    var query = {
+      username: req.params.request
+    };
+    dbo.collection("users").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      if (result.length > 0) {
+        res.json({
+          status: "success",
+          value: true
+        });
+      } else {
+        res.json({
+          status: "success",
+          value: false
+        });
+      }
+      db.close();
+    });
   });
-});
 });
 
 module.exports = router;
