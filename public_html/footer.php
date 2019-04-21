4daa35f47bb5f5b6c778105db899580ba4485544
bradennicholson <braden@bradencnicholson.com>
2019-04-21 11:12:40 -0700

    DOM fix

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,1 +1,2 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<?php echo $title; ?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php $title = "Wikit"; ?>
 
 <?php require("header.php"); ?>
 <?php require("nav.php"); ?>
 
 <body>
 
-  
+
 
 </body>
 
