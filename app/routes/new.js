1adf8604d3a0ddaec83a8a5e1f8f35308e6d0efa
bradenn <braden@bradencnicholson.com>
2019-06-19 17:01:42 -0700

    Cleanup

--- app/routes/new.js
+++ app/routes/new.js
@@ -4,7 +4,7 @@ var Posts = require('../models/textpost');
 var Realm = require('../models/realm');
 
 // GET route for reading data
-router.get('/', function(req, res, next) {
+router.get('/post', function(req, res, next) {
 
   User.findById(req.session.userId).populate("realms")
     .exec(function(error, user) {
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
                 return next(error);
               } else {
                 if (user != null) {
-                  return res.render("new", {
+                  return res.render("newpost", {
                     user: user,
                     realm: realm
                   });
@@ -37,7 +37,7 @@ router.get('/', function(req, res, next) {
 });
 
 //POST route for updating data
-router.post('/', function(req, res, next) {
+router.post('/post', function(req, res, next) {
   // confirm that user typed same password twice
   if (req.body.title &&
     req.body.section &&
@@ -83,6 +83,76 @@ router.post('/', function(req, res, next) {
   err.status = 400;
   return next(err);
 }
-})
+});
+
+// GET route for reading data
+router.get('/realm', function(req, res, next) {
+
+  User.findById(req.session.userId).populate("realms")
+    .exec(function(error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user != null) {
+          Realm.find({})
+            .exec(function(error, realm) {
+              if (error) {
+                return next(error);
+              } else {
+                if (user != null) {
+                  return res.render("newrealm", {
+                    user: user,
+                    realm: realm
+                  });
+                } else {
+
+                }
+
+              }
+            });
+        } else {
+          res.redirect("/login");
+        }
+
+      }
+    });
+});
+
+//POST route for updating data
+router.post('/realm', function(req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.name &&
+    req.body.desc) {
+    User.findById(req.session.userId)
+      .exec(function(error, user) {
+          if (error) {
+            res.redirect("/login");
+          } else {
+            if (user != null) {
+              var postData = {
+                name: req.body.name,
+                bio: req.body.desc,
+                owner: user._id,
+                nsfw: false
+              }
+              Realm.create(postData, function(error, post) {
+                if (error) {
+                  return next(error);
+                } else {
+                  return res.redirect('/r/' + post.name);
+                }
+              });
+            }
+            }
+
+      });
+
+
+} else {
+  var err = new Error('All fields required.');
+  err.status = 400;
+  return next(err);
+}
+});
 
 module.exports = router;
